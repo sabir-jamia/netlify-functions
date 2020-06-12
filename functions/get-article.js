8ae8fd68f921727cbde753f773389eba552a4ed1
Mohammad Sabir <sabir.jmi11@gmail.com>
2020-06-12 10:48:18 +0530

    test 2

--- functions/get-article.js
+++ functions/get-article.js
@@ -1,19 +1,30 @@
-exports.handler = async event => {
-   const React = require('react');
-   const babel = require('@babel/core');
-   const { renderToStaticMarkup } = require('react-dom/server');
-   const mdx = require('@mdx-js/mdx');
-   const { MDXProvider, mdx: createElement } = require('@mdx-js/react');
-   require('@babel/preset-react');
+const React = require('react');
+const babel = require('@babel/core');
+const { renderToStaticMarkup } = require('react-dom/server');
+const mdx = require('@mdx-js/mdx');
+const { MDXProvider, mdx: createElement } = require('@mdx-js/react');
+require('@babel/preset-react');
 
-   const transform = code =>
+exports.handler = async (event) => {
+   const { content: articleContent } = JSON.parse(event.body);
+   const content = await renderWithReact(articleContent);
+
+   return {
+      statusCode: 200,
+      // headers: { 'Content-Type': 'text/html' },
+      body: content,
+   };
+};
+
+const transform = (code) =>
    babel.transformSync(code, {
-      plugins : ['@babel/plugin-transform-react-jsx'],
+      plugins: ['@babel/plugin-transform-react-jsx'],
    }).code;
 
-const renderWithReact = async mdxCode => {
+const renderWithReact = async (mdxCode) => {
    const jsx = await mdx(mdxCode, { skipExport: true });
-   const code = transform(jsx);
+   // const code = transform(jsx);
+   return jsx;
    const scope = { mdx: createElement };
 
    const fn = new Function(
@@ -28,14 +39,3 @@ const renderWithReact = async mdxCode => {
 
    return renderToStaticMarkup(elementWithProvider);
 };
-
-   const { content: articleContent } = JSON.parse(event.body);
-   const content = await renderWithReact(articleContent);
-
-   return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'text/html' },
-      body: content,
-   };
-};
-
